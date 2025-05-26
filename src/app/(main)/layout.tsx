c3db4dd5de50a9485c3e68de485ce073511384c6
Prince Lartey <lartey.princ@gmail.com>
2025-05-26 13:05:06 +0000

    session context

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,11 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
+import SessionProvider from "./SessionProvider";
 
 export default async function Layout({children,}: {children: React.ReactNode;}) {
     const session = await validateRequest();
 
     if (!session.user) redirect("/login");
 
-    return <>{children}</>;
+    return <SessionProvider value={session}>{children}</SessionProvider>;
 }
